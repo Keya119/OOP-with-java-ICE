21a145a5d67b1ebd4fcbb17fffd8db01510a3a12
Keya119 <keya66505@gmail.com>
2024-10-28 00:12:59 +0600

    Create Keya.java

---
+++ Keya.java
@@ -0,0 +1,23 @@
+public class Keya{
+
+    import java.util.Scanner()
+   Scanner n= new Scanner( syestem.in);
+   public static void main( string)
+
+   {
+    int i,n.sum=0;
+    system.out.print("Enter a number: ");
+    Scanner n = new Snanner (system.in);
+    n=in.nextInt();
+    System.out.println("The odd numbers: ");
+    for(i=1;i<=n;i+=2);
+    {
+     
+        System.out.println("i");
+        sum+=i;
+
+    }
+    System.out.primtln("sum of the natural odd numbers:"+sum);
+}
+
+}
